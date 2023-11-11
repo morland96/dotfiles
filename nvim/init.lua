5daa0515189721e141fb466a26b3e68db3de08b0
Morland Meng <mmeng@atlassian.com>
2023-11-11 19:03:31 +1100

    Add yabai

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_antialiasing = true
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = true
-  vim.g.neovide_scroll_animation_length = 3
+  vim.g.neovide_scroll_animation_length = 0.3
 
   vim.o.guifont = "JetBrainsMonoNL Nerd Font Mono:h14"
   -- vim.o.guifont = "Menlo:h14"
